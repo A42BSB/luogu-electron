c573a50d701d4770025d453203519b8575f69a35
A42BSB <138642093+A42Null@users.noreply.github.com>
2026-08-11 13:12:39 +0800

    忘记加大括号了

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { app, BrowserWindow, Menu, clipboard, shell, Tray, nativeImage, dialog, globalShortcut, session, net, ipcMain } = require('electron')
 const path = require('path')
 const fs = require('fs')
-const Store = require('electron-store')
+const { Store } = require('electron-store')
 
 // ========================
 // 配置 & 状态
